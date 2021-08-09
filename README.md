63fb1db69146ec056ffeabd358ce83c58cfeb479
Brooke Hamilton <brooke.hamilton@microsoft.com>
2021-08-09 17:48:20 -0400

    test and test runner updates

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# vm
+# Bicep Test Pattern
+
+This is an example of how [Bicep](https://aka.ms/bicep) modules can be unit tested. The pattern is similar to unit testing of code. A test is written as Bicep files that consumes the module being tested. A simple test runner executes the Bicep test. The test runner can be included with the tests and designed to meet the needs of the development team.
+
+## Module Design
+
+Each module is an independently deployable, reusable, and testable unit of automation. Modules can have dependencies, e.g., testing a module that deploys a KeyVault secret requires a KeyVault to contain the secret, but dependencies are minimized in order to simplify testing of the individual modules.
+
+## Tests
+
+Each module has at least one corresponding unit test, in the form of a Bicep file. The test Bicep files perform any necessary setup and then invoke the module being tested. Test modules do not have any required parameters (but they may have parameters with defaults).
+
+All tests are in the `tests` directory and have a naming convention of `*.tests.bicep`.
+
+## Deployment Scope
+
+The modules being tested do not require a specific scope and can be reused in any deployment scope: resource group, subscription, or tenant. The test Bicep files operate in the resource group scope.
+
+## Test Runner
+
+The example test runner is a PowerShell script that invokes the Azure CLI to create a new resource group deployment for the current test being executed. Test runners can be written in whatever language you prefer. This test runner takes a parameter for the test Bicep file and creates a new resource group deployment. Below is a summary of using the test runner. Get more detailed help by running the `Get-Help` command in a PowerShell window: `Get-Help -Name .\Test-Module.ps1 -Detailed`.
+
+### Invoking a Test
+
+To run a single unit test:
+
+```PowerShell
+.\Test-Module.ps1 -ModuleFile .\tests\<test name>.tests.bicep
+```
+
+For example:
+
+```PowerShell
+.\Test-Module.ps1 -ModuleFile .\tests\keyvault.tests.bicep
+```
+
+The test runner creates a new resource group if the resource group does not already exist. Then it runs the given Bicep test file in a new resource group deployment. Errors and results are displayed when the test finishes.
+
+### Test Cleanup
+
+The test runner will delete the test resource group and all deployments when run with the `-Cleanup` switch.
+
+```PowerShell
+.\Test-Module.ps1 -Cleanup -NoWait
+```
--- Test-Module.ps1
+++ Test-Module.ps1
@@ -1,47 +1,78 @@
+<#
+.SYNOPSIS
+This is an example of a Bicep module test runner.
+
+.DESCRIPTION
+The example test runner is a PowerShell script that invokes the Azure CLI to
+create a new resource group deployment for the current test being executed.
+Test runners can be written in whatever language you prefer. This test runner
+takes a parameter for the test Bicep file and creates a new resource group
+deployment.
+
+.EXAMPLE
+PS> .\Test-Module -ModuleFile .\tests\keyvault.tests.bicep
+
+.EXAMPLE
+PS> .\Test-Module -Cleanup
+
+#>
+
+
 [CmdletBinding()]
 param (
-    
-    # Relative or absolute path of a module file containing one or more tests
+
     [Alias("m", "f")]
-    [Parameter()]
+    [Parameter(ParameterSetName = 'RunTests', Position = 0)]
     [ValidateScript( { Get-Item -Path $_ }, ErrorMessage = "ModuleFile not found.")]
     [string]
+    # Relative or absolute path of a module file containing one or more tests
     $ModuleFile,
 
-    # The name of the resource group that will contain the test deployment. The group will be created if it does not exist. The default is 'bicep-tests'.
+    [Alias("c")]
+    [Parameter(ParameterSetName = 'Cleanup', Mandatory = $true)]
+    [switch]
+    # Deletes the test resource group specified in the -ResourceGroupName parameter.
+    # Other parameters are ignored.
+    $Cleanup,
+
     [Alias("g", "r")]
-    [Parameter()]
+    [Parameter(ParameterSetName = 'RunTests')]
+    [Parameter(ParameterSetName = 'Cleanup')]
     [string]
+    # The name of the resource group that will contain the test deployment. The
+    # group will be created if it does not exist. The default is 'bicep-tests'.
     $ResourceGroupName = "bicep-tests",
 
-    # The location of the resource group and deployments. The default is 'eastus'.
     [Alias("l")]
-    [Parameter()]
+    [Parameter(ParameterSetName = 'RunTests')]
     [string]
+    # The location of the resource group and deployments. The default is 'eastus'.
     $Location = 'eastus',
 
-    # ARM Deployment mode. "Complete" or "Incremental". The default is "Incremental".
-    [Parameter()]
+    [Parameter(ParameterSetName = 'RunTests')]
     [ValidateSet("Complete", "Incremental")]
     [string]
+    # ARM Deployment mode. "Complete" or "Incremental". The default is
+    # "Incremental".
     $Mode = "Incremental",
 
-    # Name of the deployment. The default is "bicep-test-<current ticks>".
     [Alias("n")]
-    [Parameter()]
+    [Parameter(ParameterSetName = 'RunTests')]
     [string]
+    # Name of the deployment. The default is "bicep-test-<current ticks>".
     $DeploymentName = "bicep-test-$($(Get-Date).Ticks)",
 
-    # Deletes the test resource group specified in the -ResourceGroupName parameter. Other parameters are ignored.
-    [Alias("c")]
+    [Parameter(ParameterSetName = 'RunTests')]
+    [Parameter(ParameterSetName = 'Cleanup')]
     [switch]
-    $Cleanup
+    # Runs the deployment asynchronously.
+    $NoWait
 )
 
 $ErrorActionPreference = "Stop"
 
 function Log ([string]$message) {
-    Write-Host "[INFO]: $message"
+    Write-Output "[INFO]: $message"
 }
 
 function GroupExists {
@@ -50,8 +81,20 @@ function GroupExists {
 
 function Clean {
     if ($(GroupExists)) {
-        Log "Resource group '$ResourceGroupName' marked for deletion."
-        $null = $(az group delete -y --no-wait --name $ResourceGroupName)
+
+        if ($NoWait) {
+            Log "Resource group '$ResourceGroupName' marked for deletion"
+        }
+        else {
+            Log "Deleting resource group '$ResourceGroupName'"
+        }
+
+        $azArgs = "group", "delete", "--yes", "--name", $ResourceGroupName
+        if ($NoWait) {
+            $azArgs += "--no-wait"
+        }
+
+        az $azArgs
     }
     else {
         Log "Test resource group $ResourceGroupName does not exist. Nothing to clean."
@@ -71,10 +114,19 @@ function CreateTestResourceGroup {
 
 function Deploy ([bool]$async, [string]$testFile, [string]$currentDeploymentName) {
     Log "Using deployment name $currentDeploymentName"
-    Log "Running tests in $testFile"
-    
-    $command = "az deployment group create --template-file $testFile --resource-group $resourceGroupName --mode $Mode --name $currentDeploymentName --no-prompt --verbose"
-    Invoke-Expression -Command $command
+    if ($async) {
+        Log "Running tests asynchronously in $testFile"
+    }
+    else {
+        Log "Running tests and waiting for results in $testFile"
+    }
+
+    $azArgs = "deployment", "group", "create", "--verbose", "--template-file", $testFile, "--resource-group", $resourceGroupName, "--mode", $Mode, "--name", $currentDeploymentName
+    if ($async) {
+        $azArgs += '--no-wait'
+    }
+
+    az @azArgs
 }
 
 function FindTestFiles {
@@ -99,7 +151,7 @@ if (!$testFiles) {
     exit
 }
 
-$useAsync = $testFiles.Count -gt 1 # Run tests asynchronously if there are more than one.
+$useAsync = $NoWait -or ($testFiles.Count -gt 1) # Run tests asynchronously if there are more than one.
 $deploymentCount = 1
 foreach ($testFile in $testFiles) {
     Deploy -async $useAsync -testFile $testFile -currentDeploymentName "$DeploymentName-$deploymentCount"
